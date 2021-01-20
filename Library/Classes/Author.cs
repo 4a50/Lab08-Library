832fa7fab965c36e65a67b7f790b2b0c15a0e166
4a50 <jpjones22@gmail.com>
2021-01-20 15:36:59 -0800

    Added Books and Author

---
+++ Library/Classes/Author.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Library_Lab
+{
+    public class Author
+    {
+        string FirstName { get; set; }
+        string LastName { get; set; }
+
+    }
+}
---
+++ Library/Classes/Books.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Library_Lab
+{
+    public class Books
+    {
+        public string Title { get; set; }
+        public Author Author { get; set; }
+        public enum Genre
+        {
+            Fiction,
+            NonFiction,
+            AutoBiography,
+            SciFi,
+            Adventure,
+            Mystery
+        }
+    }
+}
